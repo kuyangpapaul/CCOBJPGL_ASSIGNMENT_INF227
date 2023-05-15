7c1f110d04a3281dfd332db73a074a597ad637fd
Paul Vincent C. Mercado <119905612+kuyangpapaul@users.noreply.github.com>
2023-05-15 17:14:17 +0800

    Update App.java

--- exercise3/Dog/App.java
+++ exercise3/Dog/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Create object from pet class
+      
         Cavachon myPet = new Cavachon();
 
         myPet.bark();
