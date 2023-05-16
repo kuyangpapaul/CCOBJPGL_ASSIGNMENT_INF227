9d6c270f842d53f938a46bd12ba18dc27583c4ec
Paul Vincent C. Mercado <119905612+kuyangpapaul@users.noreply.github.com>
2023-05-16 09:30:57 +0800

    Update App.java

--- exercise3/Cat/App.java
+++ exercise3/Cat/App.java
@@ -6,7 +6,8 @@ public class App {
         myPet.meow();
         System.out.println("Her eyes are an attractive shade of " + myPet.eyeColor +".");
         System.out.println("My cat weighs " + myPet.weight + " and stands " + myPet.height +" tall.");     
-        System.out.println("My cat is also very " + myPet.characteristic +" to me.");
+        System.out.println("My cat is very " + myPet.characteristic +" to me and she also loves to eat " + myPet.food + ".");
+     
 
     }
 }
